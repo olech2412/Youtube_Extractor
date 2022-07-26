4988fab512fffda8e7d03be12aa83a9b828eaadf
Ole Einar Christoph <olechristoph2412@gmail.com>
2022-07-26 13:00:06 +0200

    finally

--- Extractor.py
+++ Extractor.py
@@ -1,6 +1,15 @@
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup as bs
 
+
+def give_me_everything():
+
+    data = soup.find_all("meta")  # call everything that is in meta
+
+    for i in range(len(data)):  # print the propertys
+        print(data[i])
+
+
 video_url = "https://www.youtube.com/watch?v=5qNHtdN07FM&ab_channel=media.ccc.de"  # YouTube URL
 session = HTMLSession()  # Initialize HTML Session
 response = session.get(video_url)  # call the URL
@@ -10,5 +19,14 @@ if response.status_code != 200:  # If response != 200 don't try to read the data
 else:
     soup = bs(response.content, "html.parser")  # html parser from BeautifulSoup
 
-    var = soup.find_all("meta") # Important to know which property's you can extract
-    print(soup.find("meta", property="og:title")["content"]) # returns the content of the given property
+    # give_me_everything() # Important to know which property's you can extract
+    print("---------------------------------------Examples---------------------------------------")
+    # some examples
+    print("Title: " + soup.find("meta", property="og:title")["content"])
+    print("Application: " + soup.find("meta", property="al:ios:app_name")["content"])
+    print("Uploaddate: " + soup.find("meta", itemprop="uploadDate")["content"])
+    print("Family friendly: " + soup.find("meta", itemprop="isFamilyFriendly")["content"])
+    print("Description: " + soup.find("meta", property="og:description")["content"])
+    print("Interactions: " + soup.find("meta", itemprop="interactionCount")["content"])
+    print("Is paid: " + soup.find("meta", itemprop="paid")["content"])
+
